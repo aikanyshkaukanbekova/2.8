682a77d01e4075d83ba3d32523aa424d146b43e4
Balkhievhusein <balkhievhusein@gmail.com>
2022-10-18 00:44:51 +0300

    new

---
+++ indiv.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+def get_route():
+    """
+    Запросить данные о маршруте
+    """
+    start = input("Название начального пункта маршрута? ")
+    finish = input("Название конечного пункта маршрута? ")
+    number = int(input("Номер маршрута? "))
+
+    return {
+        'start': start,
+        'finish': finish,
+        'number': number,
+    }
+
+
+def display_route(routes):
+    """
+    Отобразить списко маршрутов
+    """
+    if routes:
+        line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+            '-' * 4,
+            '-' * 30,
+            '-' * 20,
+            '-' * 8
+        )
+        print(line)
+        print(
+            '| {:^4} | {:^30} | {:^20} | {:^15} |'.format(
+                "№",
+                "Начальный пункт",
+                "Конечный пункт",
+                "Номер маршрута"
+            )
+        )
+        print(line)
+
+        for idx, worker in enumerate(routes, 1):
+            print(
+                '| {:>4} | {:<30} | {:<20} | {:>15} |'.format(
+                    idx,
+                    worker.get('start', ''),
+                    worker.get('finish', ''),
+                    worker.get('number', 0)
+                )
+            )
+        print(line)
+    else:
+        print("Список маршрутов пуст.")
+
+
+def select_route(routes, period):
+    """
+    Выбрать маршрут
+    """
+    result = []
+    for employee in routes:
+        if employee.get('number') == period:
+            result.append(employee)
+
+    return result
+
+
+def main():
+    """
+    Главная функция программы
+    """
+    routes = []
+
+    while True:
+        command = input(">>> ").lower()
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            route = get_route()
+            routes.append(route)
+            if len(routes) > 1:
+                routes.sort(key=lambda item: item.get('number', ''))
+
+        elif command == 'list':
+            display_route(routes)
+
+        elif command.startswith('select'):
+            parts = command.split(' ', maxsplit=1)
+            period = int(parts[1])
+
+            selected = select_route(routes, period)
+            display_route(selected)
+
+        elif command == 'help':
+            print("Список команд:\n")
+            print("add - добавить маршрут;")
+            print("list - вывести список маршрутов;")
+            print("select <номер маршрута> - запросить данные о маршруте;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
---
+++ zadanie 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding^ utf-8 -*-
+
+
+def te():
+    number = int(input("Введите число: "))
+    if number > 0:
+        positive()
+    else:
+        negative()
+
+
+def positive():
+    print("Положительное")
+
+
+def negative():
+    print("Отрицательное")
+
+
+if __name__ == '__main__':
+    te()
